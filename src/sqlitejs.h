315a37fb4e51686e1d02f857f5eacaeaa0a71eea
Marco Bambini <marco@creolabs.com>
2025-04-11 08:46:22 +0200

    Moved SQLITE_JS_VERSION outside of the first ifndef

--- src/sqlitejs.h
+++ src/sqlitejs.h
@@ -5,6 +5,8 @@
 //  Created by Marco Bambini on 31/03/25.
 //
 
+#define SQLITE_JS_VERSION   "1.1.1"
+
 #ifndef __SQLITEJS__
 #define __SQLITEJS__
 
@@ -15,8 +17,6 @@
 #else
 #include "sqlite3.h"
 #endif
-
-#define SQLITE_JS_VERSION   "1.1.1"
 
 int sqlite3_js_init (sqlite3 *db, char **pzErrMsg, const sqlite3_api_routines *pApi);
 const char *sqlitejs_version (void);
