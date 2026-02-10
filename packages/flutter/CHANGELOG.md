c9acd7ae967d817dc1f3278a53a314a463ee6853
Gioele Cantoni <gioele.cantoni@gmail.com>
2026-02-10 18:03:50 +0100

    fix(workflow): missing setup dart step

--- packages/flutter/CHANGELOG.md
+++ packages/flutter/CHANGELOG.md
@@ -1,6 +1,6 @@
 # Changelog
 
-## 1.1.18
+## 1.1.19
 
 - Initial Flutter package release
 - Added support for Android 16kb page size devices
--- src/sqlitejs.h
+++ src/sqlitejs.h
@@ -16,7 +16,7 @@
 #include "sqlite3.h"
 #endif
 
-#define SQLITE_JS_VERSION   "1.1.18"
+#define SQLITE_JS_VERSION   "1.1.19"
 
 int sqlite3_js_init (sqlite3 *db, char **pzErrMsg, const sqlite3_api_routines *pApi);
 const char *sqlitejs_version (void);
